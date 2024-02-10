2e9c8efab1f9cb9e2c42c93030edfa9e2478a3cb
Sh0o0lin <balwantdod223@gmail.com>
2024-02-11 00:19:17 +0530

    skeleton setup

---
+++ src/main/java/com/shoolin/SpringKafkaMicro/SpringKafkaMicroApplication.java
@@ -0,0 +1,13 @@
+package com.shoolin.SpringKafkaMicro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaMicroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaMicroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shoolin/SpringKafkaMicro/SpringKafkaMicroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoolin.SpringKafkaMicro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaMicroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
